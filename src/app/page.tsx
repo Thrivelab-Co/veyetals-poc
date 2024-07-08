145a55d8e02b9bb3c9ca18f691ee147577dd1b5a
Marvin Calero <marvin@thrivelab.com>
2024-07-08 17:06:48 -0600

    add user experience improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,63 @@
-'use client'
-
+'use client';
 import Veyetals from "@/components/veyetals";
 import { useState } from "react";
 
 export default function Home() {
-  const [showUI, setShowUI] = useState(false);
+  const [shouldStartScanning, setShouldStartScanning] = useState(false);
+  const [shouldShowResults, setShouldShowResults] = useState(false);
+  const [jsonData, setJsonData] = useState({});
 
   const handleClick = () => {
-    setShowUI(true);
+    setShouldStartScanning(true);
   }
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl font-bold text-center">
-        Welcome to Thrivelabs POC
-      </h1>
-      { showUI && <Veyetals /> }
-      { !showUI && (
+
+  const handleComplete = (data: any) => {
+    setJsonData(data);
+    setShouldShowResults(true);
+  };
+
+  const WelcomeComponent = () => {
+    return (
+      <view className="flex flex-col p-24 justify-between min-h-screen">
+        <h1 className="text-3xl font-bold text-center">
+          Welcome to Thrivelabs POC
+        </h1>
         <button className="p-4 rounded bg-indigo-400 cursor-pointer" onClick={handleClick}>
           Start by clicking here
         </button>
-      )}
-      <div className="flex flex-col items-center space-y-4">
-        <p className="text-lg text-center">
-          This is a proof of concept for Thrivelab.
-        </p>
-      </div>
+        <div className="flex flex-col items-center space-y-4">
+          <p className="text-lg text-center">
+            This is a proof of concept for Thrivelab.
+          </p>
+        </div>
+      </view>
+    );
+  }
 
+  const ResultsComponent = () => {
+    return (
+      <view className="flex flex-col p-24 gap-6 min-h-screen">
+        <h1 className="text-3xl font-bold text-center">
+          Results
+        </h1>
+        <div className="flex flex-col items-center space-y-4">
+          <pre className="mt-4 p-4 rounded text-left overflow-auto">
+            <code>
+              {JSON.stringify(jsonData, null, 2)}
+            </code>
+          </pre>
+        </div>
+      </view>
+    );
+  }
+
+  const VeyetalsComponent = () => {
+    return shouldShowResults ? <ResultsComponent/> : <Veyetals onComplete={handleComplete}/>;
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      {shouldStartScanning ? <VeyetalsComponent /> : <WelcomeComponent />}
     </main>
   );
 }
--- src/components/veyetals.tsx
+++ src/components/veyetals.tsx
@@ -1,11 +1,7 @@
 import VeyetalsView from '@veyetals/react-veyetals-view'
 import '@veyetals/react-veyetals-view/dist/index.css'
 
-const Veyetals = () => {
-  const onComplete = (data: any) => {
-    console.log('onComplete', data);
-  }
-
+const Veyetals = ({onComplete}: {onComplete: (data: any) => void}) => {
   const userId = 'test-user-id-' + new Date().getTime();
   const channelId = 'THRIVE_LABS';
 
